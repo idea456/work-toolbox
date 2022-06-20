fa6e0b60562893dc8ddc82efe9a085a6f3c3c8d8
Adrienne Rio <riowongsoatmojo@gmail.com>
2022-06-20 06:00:59 +0000

    Added cron

--- cmd/remind/add.go
+++ cmd/remind/add.go
@@ -5,23 +5,28 @@ package remind
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
+
+	"encoding/json"
 
 	"github.com/spf13/cobra"
 )
 
 type By string
+
 const (
-	TODAY = "today"
-	TOMORROW = "tomorrow"
+	TODAY     = "today"
+	TOMORROW  = "tomorrow"
 	THIS_WEEK = "this-week"
 )
 
 type Reminder struct {
-	title string
-	message string
-	by By
-	postedOn time.Time
+	Title    string
+	Message  string
+	By       By
+	PostedOn time.Time
 }
 
 // global variables for arguments
@@ -29,22 +34,63 @@ var (
 	by string
 )
 
-
 func addReminder() {
 
+}
+
+func remind(title string, message string, by By) {
+	reminder := Reminder{
+		Title:    title,
+		Message:  message,
+		By:       by,
+		PostedOn: time.Now(),
+	}
+
+	bytes, _ := json.Marshal(reminder)
+	fmt.Println(string(bytes))
+}
+
+func openSchedule() *os.File {
+	file, err := os.OpenFile("schedule.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Unable to read schedules: ^%v", err)
+	}
+	return file
 }
 
 // addCmd represents the add command
 var AddCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new reminder to notify myself later",
-	Long: `Add a new reminder to notify myself later.`,
+	Long:  `Add a new reminder to notify myself later.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
+		reminder := Reminder{
+			Title:    "Code review",
+			Message:  "this link",
+			By:       TODAY,
+			PostedOn: time.Now(),
+		}
+
+		file := openSchedule()
+		r, _ := json.MarshalIndent(reminder, "", "")
+
+		// s := gocron.NewScheduler(time.UTC)
+
+		// s.Every(2).Seconds().Do(func() {
+		// 	remind("Code Review", "this link", TODAY)
+		// })
+
+		defer file.Close()
+
+		if _, err := file.WriteString(string(r)); err != nil {
+			panic(err)
+		}
+
+		// s.StartBlocking()
 	},
 }
 
 func init() {
 
-	AddCmd.Flags().StringVarP(&by, "by", "t", "today", "Set the reminder by a certain time period, available options are: 12-HR time period, today, tomorrow, this-week")
+	AddCmd.Flags().StringVarP(&by, "by", "t", "today", "Set the reminder by HH:MM, available options are: 12-HR time period, today, tomorrow, this-week")
 }
--- go.mod
+++ go.mod
@@ -3,21 +3,27 @@ module github.com/idea456/work-toolbox
 go 1.18
 
 require (
+	github.com/spf13/cobra v1.4.0
+	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31
+	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb
+	google.golang.org/api v0.84.0
+)
+
+require (
 	cloud.google.com/go/compute v1.7.0 // indirect
+	github.com/go-co-op/gocron v1.14.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20220617184016-355a448f1bc9 // indirect
-	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb // indirect
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/api v0.84.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
 	google.golang.org/grpc v1.47.0 // indirect
